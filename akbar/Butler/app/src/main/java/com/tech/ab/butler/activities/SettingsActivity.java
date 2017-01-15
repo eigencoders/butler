b1f8e88fcaecd7b62ad98dd1cd1801c5809ca3a1
ankita-kalra <technocrat.ankita@gmail.com>
2017-01-15 20:20:40 +0530

    Completed previous two commits. Apologies for multiple.

---
+++ akbar/Butler/app/src/main/java/com/tech/ab/butler/activities/SettingsActivity.java
@@ -0,0 +1,114 @@
+package com.tech.ab.butler.activities;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.tech.ab.butler.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    TextView tvAddPlaces, tvScheduleName;
+    EditText etAddPlaces,etScheduleName;
+    final Activity context=this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        tvAddPlaces=(TextView)findViewById(R.id.tvAddPlaces);
+        tvScheduleName=(TextView)findViewById(R.id.tvScheduleName);
+        etAddPlaces=(EditText)findViewById(R.id.etAddPlaces);
+        etScheduleName=(EditText)findViewById(R.id.etScheduleName);
+
+        tvAddPlaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.add_place_dialog, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+                alertDialogBuilder.setView(promptsView);
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.etAddPlaces);
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Add",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        etAddPlaces.setText(userInput.getText());
+                                        /*TODO-
+                                          Populate a string array accordingly.
+                                         */
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
+
+
+        tvScheduleName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.schedule_name_dialog, null);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+                alertDialogBuilder.setView(promptsView);
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.etScheduleName);
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Change",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        etScheduleName.setText(userInput.getText());
+                                        /*TODO-
+                                          Maintain a variable for this.
+                                         */
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+
+            }
+        });
+
+
+    }
+}
