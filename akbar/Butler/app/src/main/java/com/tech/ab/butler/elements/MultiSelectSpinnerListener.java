2402457902967eef5fa8d56d291dbf1f80100d03
ankita-kalra <technocrat.ankita@gmail.com>
2017-01-26 17:25:52 +0530

    Resolved Issue in MultiSelectSpinner. Created separate class for DatePicker and Time Picker.

---
+++ akbar/Butler/app/src/main/java/com/tech/ab/butler/elements/MultiSelectSpinnerListener.java
@@ -0,0 +1,9 @@
+package com.tech.ab.butler.elements;
+
+/**
+ * Created by Ankita on 26-Jan-17.
+ */
+
+public interface MultiSelectSpinnerListener {
+    void onItemsSelected(boolean[] selected);
+}
---
+++ akbar/Butler/app/src/main/java/com/tech/ab/butler/elements/TimePickerDialogFragment.java
@@ -0,0 +1,34 @@
+package com.tech.ab.butler.elements;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Ankita on 26-Jan-17.
+ */
+
+public class TimePickerDialogFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // TODO - Do something with time
+    }
+}
